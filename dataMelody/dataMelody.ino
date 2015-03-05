1977c2bdf3a74fe4f0be648895f00b7920f83f8d
extrasleepy <akleindolph@gmail.com>
2015-03-05 10:22:08 -0800

    pin changed

--- dataMelody/dataMelody.ino
+++ dataMelody/dataMelody.ino
@@ -2,8 +2,7 @@
 //circuit: 8-ohm speaker on digital pin 3 
 
 // paste data in { }brackets:
-int melody[] = {
-  1000};
+int melody[] = {/*your data here*/};
 
 void setup() {
 }
@@ -11,10 +10,11 @@ void setup() {
 void loop() {
                                 // iterate over the notes of the melody:
   for (int note = 0; note < sizeof(melody); note++) {
-    tone(3, melody[note]/30,melody[note]/30);
+    //melody[note]=map(melody[note],0,30000,1000,10000);
+    tone(8, melody[note]/30,melody[note]/30);
     delay(100);
     //delay(melody[note]/100);  //optional adjustment to delay
-    noTone(3);
+    noTone(8);
   }
   delay(1000);                //one second before song repeats
 }
